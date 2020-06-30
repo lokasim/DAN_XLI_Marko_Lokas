eb9bcab3e1a66e8aa105b5d877b661a1a2d79e8a
markolokas <lokasmarko.im@gmail.com>
2020-06-30 08:13:39 +0200

    Commenting code...

--- DAN_XLI_Marko_Lokas/PrintDoc/MainWindow.xaml.cs
+++ DAN_XLI_Marko_Lokas/PrintDoc/MainWindow.xaml.cs
@@ -33,13 +33,12 @@ namespace PrintDoc
                 WorkerReportsProgress = true,
                 WorkerSupportsCancellation = true
             };
-    public string textPanelString;
+        public string textPanelString;
         public string numberCopiesString;
         public string currentTextPanel;
         public string currentNumberCopies;
         public int sumCopies = 0;
-
-
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -48,13 +47,16 @@ namespace PrintDoc
             txtTextPanel.Focus();
             notice.Visibility = Visibility.Visible;
             Print();
-            
-
         }
 
+        /// <summary>
+        /// Starting BackgroundWorker
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnPrint_Click(object sender, RoutedEventArgs e)
         {
-            
+            //If it is started, it prints a message that it has already started
             if (bw.IsBusy)
             {
                 MessageBox.Show("Printing is already in progress. Please wait...","Printing...", MessageBoxButton.OK, MessageBoxImage.Information);
@@ -74,28 +76,26 @@ namespace PrintDoc
 
                 notice.Visibility = Visibility.Collapsed;
 
-
                 bw.DoWork += bw_DoWork;
                 bw.ProgressChanged += bw_ProgressChanged;
                 bw.RunWorkerCompleted += bw_RunWorkerCompleted;
                 bw.RunWorkerAsync();
                 btnStopPrint.IsEnabled = true;
             }
-            
-
-            //bw.DoWork += bw_DoWork;
-            //bw.ProgressChanged += bw_ProgressChanged;
-            //bw.RunWorkerCompleted += bw_RunWorkerCompleted;
-            //bw.RunWorkerAsync("Hello to worker");
-            
         }
 
+        /// <summary>
+        /// An indicator of what it is doing in the background
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void bw_DoWork(object sender, DoWorkEventArgs e)
         {
             sumCopies = 0;
             int percentage = 100 / Convert.ToInt32(numberCopiesString);
             for (int i = 0; i <= 100; i += percentage)
             {
+                //Duration of the printing process
                 Thread.Sleep(1000);
                 sumCopies = sumCopies + 1 ;
                 if (i % 3 == 0)
@@ -104,13 +104,11 @@ namespace PrintDoc
                     if (i == 99)
                     {
                         bw.ReportProgress(100);
-
                     }
                 }
                 else
                 {
                     bw.ReportProgress(i);
-
                 }
                 if (bw.CancellationPending)
                 {
@@ -118,6 +116,7 @@ namespace PrintDoc
                     bw.ReportProgress(0);
                     return;
                 }
+                //Naming a storage document
                 DateTime date = DateTime.Now;
                 string dateString = date.ToString("dd_MM_yyyy_hh_mm");
                 string nameDocument = @"..\..\Printed\" + sumCopies + "." + dateString + ".txt";
@@ -125,15 +124,19 @@ namespace PrintDoc
                 if (Convert.ToInt32(numberCopiesString) >= sumCopies)
                 {
                     CreateCopies(nameDocument);
-
                 }
-
             }
             //To always finish 100%
+            //Due to the number of copies that leave a remainder when divided by 100.
             bw.ReportProgress(100);
             e.Result = "Finish printing";
         }
 
+        /// <summary>
+        /// State BackgroundWorker
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             if (e.Cancelled)
@@ -148,26 +151,35 @@ namespace PrintDoc
             else
             {
                 lblProgressBar.Content = ("Successfully completed: " + e.Result);
+                //When the printing process is complete, the stop printing button is disabled
                 btnStopPrint.IsEnabled = false;
             }
         }
 
+        /// <summary>
+        /// Notification to the user of what is happening in the background of the process
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             progressBar.Value = e.ProgressPercentage;
             if (Convert.ToInt32(numberCopiesString) < sumCopies)
             {
                 lblProgressBar.Content = "The printing process is soon over...";
-
             }
             else
             {
                 lblProgressBar.Content = "Printing copies " + sumCopies;
             }
-
             lblPercentage.Content  =  ("Completed " + e.ProgressPercentage + "%");
         }
 
+        /// <summary>
+        /// Button that stops the process of creating files (background worker)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnStopPrint_Click(object sender, RoutedEventArgs e)
         {
             if (bw.IsBusy)
@@ -177,11 +189,19 @@ namespace PrintDoc
                 lblPercentage.Content = "Stopped the printing process...";
             }
         }
-        
 
+        /// <summary>
+        /// Method for creating copies
+        /// </summary>
+        /// <param name="location"></param>
         private void CreateCopies(string location)
         {
-            //File.Create(location);
+            //If there is no folder, create it
+            if (!Directory.Exists(@"..\..\Printed"))
+            {
+                Directory.CreateDirectory(@"..\..\Printed");
+            }
+            //Creates a new file
             using (StreamWriter outputFile = new StreamWriter(location))
             {
 
@@ -189,8 +209,13 @@ namespace PrintDoc
             }
         }
 
+        /// <summary>
+        /// Visual indicators of a correctly filled field
+        /// </summary>
         private void InputText(object sender, TextChangedEventArgs e)
         {
+            tbCapsLock.Visibility = Visibility.Collapsed;
+
             if (txtTextPanel.Text.Length < 1)
             {
                 txtTextPanel.BorderBrush = new SolidColorBrush(Colors.Red);
@@ -204,9 +229,11 @@ namespace PrintDoc
                 textPanel = true;
             }
             Print();
-
         }
 
+        /// <summary>
+        /// Visual indicators of a correctly filled field
+        /// </summary>
         private void NumberCopies(object sender, TextChangedEventArgs e)
         {
             if (txtNumOfCopies.Text.Length < 1)
@@ -228,33 +255,44 @@ namespace PrintDoc
             Print();
         }
 
+        /// <summary>
+        /// Checks that the input fields are filled in correctly and enables the print button accordingly
+        /// </summary>
         private void Print()
         {
-            if (textPanel && numberCopies)
+            //Everything is OK
+            if (textPanel && numberCopies && Convert.ToInt16(txtNumOfCopies.Text) != 0)
             {
                 btnPrint.IsEnabled = true;
                 notice.Foreground = Brushes.Green;
                 notice.Text = "Everything is ready to print, press the button.".ToString();
+                arrow.Visibility = Visibility.Visible;
             }
+            //If a field is not filled in correctly
             else
             {
                 btnPrint.IsEnabled = false;
                 notice.Foreground = Brushes.Red;
+                notice.Visibility = Visibility.Visible;
+
+                arrow.Visibility = Visibility.Collapsed;
+
                 notice.Text = "Notice: \nEnter the desired text in the text input field, " +
                             "then the number of copies. " +
                             "After you have done this, press the \"Print\" " +
                             "button to start the document printing simulation.text input field...".ToString();
-
             }
         }
 
+        /// <summary>
+        /// Prohibits space input
+        /// </summary>
         private void TxtBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             e.Handled = e.Key == Key.Space;
             if (e.Key == Key.Space)
             {
                 tbCapsLock.Visibility = Visibility.Visible;
-                //
                 error.Text = "Try without spaces.";
                 SystemSounds.Beep.Play();
             }
@@ -264,6 +302,9 @@ namespace PrintDoc
             }
         }
 
+        /// <summary>
+        /// Field number of copies, receives numbers only
+        /// </summary>
         private Boolean DigitAllowed(String s)
         {
             foreach (Char c in s.ToCharArray())
@@ -284,12 +325,19 @@ namespace PrintDoc
             return true;
         }
 
+        /// <summary>
+        /// It forbids entering anything that is not a number
+        /// </summary>
         private void PreviewTextInputHandlerDigit(Object sender, TextCompositionEventArgs e)
         {
             e.Handled = !DigitAllowed(e.Text);
             MaxNumberCopies();
         }
 
+        /// <summary>
+        /// The maximum number of characters is 4 
+        /// in the number of copies field
+        /// </summary>
         public async void MaxNumberCopies()
         {
             int duration = 5000;
